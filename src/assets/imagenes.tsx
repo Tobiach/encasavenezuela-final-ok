2c602a695cf26f6ff1263377698bae614a567edc
Tobiach <esenciezen@gmail.com>
2026-03-16 23:52:19 -0300

    fix: Add missing src/assets/imagenes.tsx

---
+++ src/assets/imagenes.tsx
@@ -0,0 +1,36 @@
+import { getSupabaseImageUrl } from '../../lib/supabaseImageUrl';
+
+export const LOGO_ENCASA_IMAGE = getSupabaseImageUrl('assets/logo_encasavenezuela.jpg');
+
+// Hero Banners
+export const HERO_SLIDE_1 = getSupabaseImageUrl('assets/hero/banner_1.png');
+export const HERO_SLIDE_2 = getSupabaseImageUrl('assets/hero/banner_2.png');
+export const HERO_SLIDE_3 = getSupabaseImageUrl('assets/hero/banner_3.png');
+
+export const BANNER_IMAGES = {
+    banner1: HERO_SLIDE_1,
+    banner2: HERO_SLIDE_2,
+    banner3: HERO_SLIDE_3,
+};
+
+// Product Images
+export const IMG_FLIPS = getSupabaseImageUrl('imagenes_productos/flips_dulcedeleche_1.png');
+export const IMG_CERELAC = getSupabaseImageUrl('imagenes_productos/cerelac.png');
+export const IMG_SAVOY_CARRE = getSupabaseImageUrl('imagenes_productos/chocolate_savoy_carre.png');
+export const IMG_DONA_AREPA = getSupabaseImageUrl('imagenes_productos/harina_dona_arepa.png');
+export const IMG_PIRULIN = getSupabaseImageUrl('imagenes_productos/pirulin.png');
+export const IMG_MALTA_58 = getSupabaseImageUrl('imagenes_productos/lata_de_58_malta_artesanal.png');
+export const IMG_REKO_MALTA = getSupabaseImageUrl('imagenes_productos/re-ko_malta.png');
+export const IMG_REKOLITA = getSupabaseImageUrl('imagenes_productos/re-kolita.png');
+export const IMG_RON_1796 = getSupabaseImageUrl('imagenes_productos/botella_de_ron_santa_teresa_1796.png');
+export const IMG_CRICRI = getSupabaseImageUrl('imagenes_productos/savoy_cricri.png');
+export const IMG_SAMBA = getSupabaseImageUrl('imagenes_productos/savoy_samba.png');
+export const IMG_SUSY = getSupabaseImageUrl('imagenes_productos/susy.png');
+export const IMG_FAMA_AMERICA = getSupabaseImageUrl('imagenes_productos/cafe_fama_de_america.png');
+export const IMG_CAJA_PIRULIN = getSupabaseImageUrl('imagenes_productos/caja_de_pirulin.png');
+export const IMG_MARILU = getSupabaseImageUrl('imagenes_productos/chocolate_marilu.png');
+export const IMG_CHULETA = getSupabaseImageUrl('imagenes_productos/chuleta_ahumada.png');
+export const IMG_HARINA_PAN = getSupabaseImageUrl('imagenes_productos/harina_pan.png');
+export const IMG_CACHAPA = getSupabaseImageUrl('imagenes_productos/elindio_cachapas_de_maiz.png');
+export const IMG_FORORO = getSupabaseImageUrl('imagenes_productos/fororo_harina_de_maiz.png');
+export const IMG_PRESTIGIO = getSupabaseImageUrl('imagenes_productos/caja_chocolate_prestigio.png');
